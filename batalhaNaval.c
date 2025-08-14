6af735c11d90be3f0a1e4e0d139222cec8e052e3
yuribeletti <80957457+yuribeletti@users.noreply.github.com>
2025-08-14 23:45:52 +0000

    update com correções sugeridas

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,36 +1,40 @@
 #include <stdio.h>
 
 int main() {
-int tabuleiro[10][10] = { {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                            };
+    int tabuleiro[10][10];
 
-    // Navio verticalmente na coluna 0
-    for (int i = 0; i < 3; i++) { 
-        tabuleiro[i][0] = 1;
+    // Inicializando
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            tabuleiro[i][j] = 0;
+        }
     }
 
-    // Navio horizontal na linha 4
+    // Posição dos navios
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[i][0] = 3;
+    }
     for (int j = 3; j < 6; j++) {
-        tabuleiro[4][j] = 1;
+        tabuleiro[4][j] = 3;
     }
 
-    // Coordenadas dos navios
-    printf("Coordenadas dos navios:\n");
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 6; j++) {
-            if (tabuleiro[i][j] == 1) {
+    // Print dos navios
+    printf("Coordenadas dos navios (valor 3):\n");
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            if (tabuleiro[i][j] == 3) {
                 printf("Navio na posição (%d, %d)\n", i, j);
             }
         }
     }
 
-    // Print do tabuleiro
-    printf("\nTabuleiro:\n");
+    // Print do tabuleiro completo
+    printf("\nTabuleiro (0 = água, 3 = navio):\n");
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
             printf("%d ", tabuleiro[i][j]);
         }
-        printf("\n"); 
+        printf("\n");
     }
 
     return 0;
